b3ebdd3b75297092eccfda1f0ad67729c2e682b5
Daffy <vivian@hausofwong.com>
2026-09-26 16:41:11 +1000

    upd: cloudflared daffy

--- hosts/daffy/user.nix
+++ hosts/daffy/user.nix
@@ -1,4 +1,4 @@
-{ pkgs, ... }: {
+{ pkgs, lib, ... }: {
 
  
 
@@ -6,14 +6,14 @@
     cloudflared = {
       enable = true;
       tunnels."0b528b07-cb3a-43f2-b49b-e7155fba5b9e" = {
-        credentialsFile = ;
+        credentialsFile = ./credFile.json;
         default = "http_status:404" ;
       };
     };
     openssh = {
       allowSFTP = false;
-      settings = { 
-        KbdInteractiveAuthentication = true;
+      settings = lib.mkForce { 
+        KbdInteractiveAuthentication =  true;
         PasswordAuthentication = false;
       }; 
     };
@@ -23,11 +23,11 @@
     enable = true;
     admins = [ "vivian@hausofwong.com" ];
     virtualHosts."chat.toadhog.com" = {
-      enable = true;
+      enabled = true;
       domain = "chat.toadhog.com";
     };
     muc = [ { domain = "muc.chat.toadhog.com"; } ];
-    uploadHttp = {
+    httpFileShare = {
       domain = "upload.chat.toadhog.com";
     };
   };
